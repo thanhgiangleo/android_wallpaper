7168ee163a12015d85c07250e5a57173cffaa829
Dinh Trang Thanh Giang <thanhgiang.hcmus@gmail.com>
2018-05-16 10:58:46 +0700

    done uploading category

--- admin/js/app.js
+++ admin/js/app.js
@@ -4,14 +4,14 @@
 		authDomain: "wallpaper-dd7d1.firebaseapp.com",
 		databaseURL: "https://wallpaper-dd7d1.firebaseio.com",
 		projectId: "wallpaper-dd7d1",
-		storageBucket: "",
+		storageBucket: "wallpaper-dd7d1.appspot.com",
 		messagingSenderId: "390687655711"
 	};
 
 	firebase.initializeApp(config);
 	firebase.auth.Auth.Persistence.LOCAL;
 
-// handle login btn
+// handle login btn + logout
 	$("#btn-login").click(function() {
 		var email = $("#email").val();
 		var pass =  $("#password").val();
@@ -20,7 +20,20 @@
 		result.catch(function(error){
 			var errCode = error.code;
 			var errMsg = error.message;
-
 			console.log(errCode + " : " + errMsg);
 		});
 	});
+
+	$("#btn-logout").click(function() {
+		firebase.auth().signOut();
+	});
+
+// switch views
+	function switchView(v) {
+		$.get({
+			url: v,
+			cache: false
+		}).then(function(data) {
+			$("#container").html(data);
+		});
+	}
